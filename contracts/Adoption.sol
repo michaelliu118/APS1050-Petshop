0b7f575cc656be0f839f5ee9543785d12854b8b1
abamrah <71141583+abamrah@users.noreply.github.com>
2022-04-12 21:35:26 -0400

    Updated

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract Adoption {
 	address[16] public adopters;
 
-
+  
   // Adopting a pet
   function adopt(uint petId) public returns (uint) {
     require(petId >= 0 && petId <= 15);
@@ -18,78 +18,77 @@ contract Adoption {
     return adopters;
   }
   
-  //Returning a pet
-  function returnPer(uint petId) public returns(uint){
-    require(petId >=0 && petId <=15);
-    
-	//if the animal has been adopted by msg.sender, the animal can be returned
-	if (adopters[petId]==msg.sender){
-		//"Return" an animal by setting the address of it's adopter back to 0
-		adopters[petId]=address(0);
-	}
 
-	return petId;    
+  function returnPet(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
+
+    // If the animal has been adopted by msg.sender, the pet can be returned
+    if (adopters[petId] == msg.sender) {
+      // "Return" an pet by setting the address of it's adopter back to 0
+      adopters[petId] = address(0);
+    }
+    return petId;
   }
 
   // Contract for the new voting feature.
-  // Model a Candidate
-  struct Candidate {
-	  uint id;
-	  string name;
-	  uint voteCount;
-  }
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-  // Store accounts that have voted
-  mapping(address => bool) public voters;
-  // Store Candidates
-  // Fetch Candidate
-  mapping(uint => Candidate) public candidates;
-  // Store Candidates Count
-  uint public candidatesCount;
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+	// Store Candidates
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates Count
+	uint public candidatesCount;
 
-  // voted event
-  event votedEvent (
-	  uint indexed _candidateId
-  );
+	// voted event
+	event votedEvent (
+		uint indexed _candidateId
+	);
 
-  constructor () public {
-	  addCandidate("Frieda");
-	  addCandidate("Gina");
-	  addCandidate("Collins");
-	  addCandidate("Melissa");
-	  addCandidate("Jeanine");
-	  addCandidate("Elvia");
-	  addCandidate("Latisha");
-	  addCandidate("Coleman");
-	  addCandidate("Nichole");
-	  addCandidate("Fran");
-	  addCandidate("Leonor");
-	  addCandidate("Dean");
-	  addCandidate("Stevenson");
-	  addCandidate("Kristina");
-	  addCandidate("Ethel");
-	  addCandidate("Terry");
-  }
-  
-  function addCandidate (string memory _name) private {
-	  candidatesCount ++;
-	  candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-  }
-  
-  function vote (uint _candidateId) public {
-	  // require that they haven't voted before
-	  require(!voters[msg.sender]);
-	  
-	  // require a valid candidate
-	  require(_candidateId > 0 && _candidateId <= candidatesCount);
-	  
-	  // record that voter has voted
-	  voters[msg.sender] = true;
-	  
-	  // update candidate vote Count
-	  candidates[_candidateId].voteCount ++;
-	  
-	  // trigger voted event
-	  emit votedEvent(_candidateId);
-  }
+	constructor () public {
+		addCandidate("Frieda");
+		addCandidate("Gina");
+		addCandidate("Collins");
+		addCandidate("Melissa");
+		addCandidate("Jeanine");
+		addCandidate("Elvia");
+		addCandidate("Latisha");
+		addCandidate("Coleman");
+		addCandidate("Nichole");
+		addCandidate("Fran");
+		addCandidate("Leonor");
+		addCandidate("Dean");
+		addCandidate("Stevenson");
+		addCandidate("Kristina");
+		addCandidate("Ethel");
+		addCandidate("Terry");
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+		// require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		// record that voter has voted
+		voters[msg.sender] = true;
+
+		// update candidate vote Count
+		candidates[_candidateId].voteCount ++;
+
+		// trigger voted event
+		emit votedEvent(_candidateId);
+	}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,23 @@
 App = {
   web3Provider: null,
   contracts: {},
-
   // the current user account number.
-  account: '0x0',
-  hasVoted: false,
+  account: '0x0', //arsh's addtion
+  hasVoted: false, //arsh's addition
 
   init: async function() {
     // Load pets.
     $.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
+
+      var breedDropdown = $("#breedFilter");
+      var breedList = [];
+      var ageDropdown = $("#ageFilter");
+      var ageList = [];
+      var locationDropdown = $("#locationFilter");
+      var locationList = [];
+
 
       for (i = 0; i < data.length; i ++) {
         petTemplate.find('.panel-title').text(data[i].name);
@@ -19,9 +26,49 @@ App = {
         petTemplate.find('.pet-age').text(data[i].age);
         petTemplate.find('.pet-location').text(data[i].location);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        petTemplate.find('.btn-return').attr('data-id', data[i].id);
+
+        breedList.push(data[i].breed);
+        ageList.push(data[i].age);
+        locationList.push(data[i].location);
 
         petsRow.append(petTemplate.html());
       }
+      breedList = breedList.filter((item, i, ar) => ar.indexOf(item) === i);
+      ageList = ageList.filter((item, i, ar) => ar.indexOf(item) === i);
+      locationList = locationList.filter((item, i, ar) => ar.indexOf(item) === i);
+      breedList.forEach(function(e, i){
+        breedDropdown.append($('<option></option>').text(e)); 
+      });
+      ageList.forEach(function(e, i){
+        ageDropdown.append($('<option></option>').text(e)); 
+      });
+      locationList.forEach(function(e, i){
+        locationDropdown.append($('<option></option>').text(e)); 
+      });
+
+      //The filter for pets
+      $(document).on('click', '.btn-filter', function(){
+        var theBreed = document.getElementById("breedFilter").value;
+        var theAge = document.getElementById("ageFilter").value;
+        var theLocation = document.getElementById("locationFilter").value;
+        petsRow.empty();
+        
+        console.log(theLocation=="All")
+        for (i = 0; i < data.length; i ++) {
+          //console.log(i)
+          if ((data[i].breed==theBreed || theBreed=="All") && (data[i].age==theAge || theAge=="All") && (data[i].location==theLocation || theLocation=="All")){
+            petTemplate.find('.panel-title').text(data[i].name);
+            petTemplate.find('img').attr('src', data[i].picture);
+            petTemplate.find('.pet-breed').text(data[i].breed);
+            petTemplate.find('.pet-age').text(data[i].age);
+            petTemplate.find('.pet-location').text(data[i].location);
+            petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+            petsRow.append(petTemplate.html());
+          }
+        }
+      });
     });
 
     return await App.initWeb3();
@@ -32,8 +79,7 @@ App = {
       App.web3Provider = window.ethereum;
       try {
         // Request account access
-        //await window.ethereum.request({ method: "eth_requestAccounts" });;
-        await window.ethereum.enable();
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
       } catch (error) {
         // User denied account access...
         console.error("User denied account access")
@@ -48,19 +94,18 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
-
-    /**
-    * Feature: Show current user and balance.
-    */
-     web3.eth.getAccounts(function(error, accounts) {
-      if (error) { 
+        /**
+        * Feature: Show current user and balance. Arsh
+        */
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
         console.log(error);
       }
-      
       const account = accounts[0];
       $('.currUser').text("Current user: " + account);
       // Store the current account number to the gobal variable.
       App.account = accounts[0]
+      //Arsh's Addition
       web3.eth.getBalance(account, function(error, balance) {
           if (error) {
             console.log(error);
@@ -69,8 +114,6 @@ App = {
           $('.currBalance').text("Current balance: " + currBalance + ' ETH')
       });
     });
-
-
     return App.initContract();
   },
 
@@ -83,8 +126,8 @@ App = {
       // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
 
-      // update info after user made a vote.
-      App.listenForEvents()
+      // update info after user made a vote. Arsh
+      App.listenForEvents();
     
       // Use our contract to retrieve and mark the adopted pets
       return App.markAdopted();
@@ -95,9 +138,11 @@ App = {
 
   bindEvents: function() {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
+    //binding return pet
+    $(document).on("click", '.btn-return', App.handleReturn);
   },
 
-  // Listen for events emitted from the contract
+  // Listen for events emitted from the contract Arsh
   listenForEvents: function() {
     App.contracts.Adoption.deployed().then(function(instance) {
       instance.votedEvent({}, {
@@ -114,11 +159,12 @@ App = {
 
   markAdopted: function(adopters, account) {
     var adoptionInstance;
-    // for counting and voting
+    
+    // for voting and count of adopted and clients visited Arsh
     var num_adopted = 0;
     var clients = new Set();
 
-    // for voting.
+
     var content = $("#content");
     content.hide();
     $('#voteDropList').hide();
@@ -131,22 +177,25 @@ App = {
     $('#petsAdoptedNum').text("Total number of pets adopted: " + num_adopted)
     $('#clientsAdoptedNum').text("Total number of clients: " + clients.size)
 
-
     App.contracts.Adoption.deployed().then(function(instance) {
       adoptionInstance = instance;
 
       return adoptionInstance.getAdopters.call();
     }).then(function(adopters) {
       for (i = 0; i < adopters.length; i++) {
+        
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-          // counting and incrementing
-          num_adopted++;
-          clients.add(adopters[i])
-          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+          
+          num_adopted++; //increment count Arsh
+          clients.add(adopters[i])  //increment count Arsh
+
+          $(".panel-pet").eq(i).find(".btn-adopt").text('Success').attr('disabled', true);
+          $(".panel-pet").eq(i).find(".btn-return").removeProp("disabled").addClass("btn-danger");
+          $(".panel-pet").eq(i).find(".adopter-address").html(adopters[i]);
         }
       }
 
-      // Show info (balance, pets adopted so far) for the current user.
+      // Show info (balance, pets adopted so far) for the current user Arsh.
       web3.eth.getAccounts(function(error, accounts) {
         // accounts[0] is the current user.
         if (error) {
@@ -166,7 +215,7 @@ App = {
             }
             var currBalance = web3.fromWei(balance.toNumber(), 'Ether')
             $('.currBalance').text("Current balance: " + currBalance + ' ETH')
-            // show the candidate pets ballot option if currentBalance > 0 (allow the user to vote).
+            // show the candidate pets ballot option if currentBalance > 0 (allow the user to vote). Arsh
             if (currBalance > 0) {
                 var candArray = [];
                 for (var i = 1; i <= 16; i++) { // candidatesCount=16
@@ -225,7 +274,7 @@ App = {
       $("#petsAdoptedNum").text("Total number of pets adopted: " + num_adopted)
       $("#clientsAdoptedNum").text("Total number of clients: " + clients.size)
 
-
+      
     }).catch(function(err) {
       console.log(err.message);
     });
@@ -236,35 +285,14 @@ App = {
 
     var petNameList = ["Frieda", "Gina", "Collins", "Melissa", "Jeanine", "Elvia",
                        "Latisha", "Coleman", "Nichole", "Fran", "Leonor", "Dean",
-                       "Stevenson", "Kristina", "Ethel", "Terry"];
+                       "Stevenson", "Kristina", "Ethel", "Terry"];  //Arsh
 
     var petId = parseInt($(event.target).data('id'));
     var petName = petNameList[petId];
-    /**
-    * Feature 1: Disable button during processing
-    *
-    */
-    // $(this).text('Signing...').attr('disabled', true);
-    $('.panel-pet').eq(petId).find('button').text('Signing...').attr('disabled', true);
 
-    // Prevent users from adopting pet if they cannot afford the handling fee (gas fee).
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-      web3.eth.getBalance(accounts[0], function(error, balance) {
-          if (error) {
-            console.log(error);
-          }
-          var currBalance = web3.fromWei(balance.toNumber(), 'Ether')
-          if (currBalance==0) {
-              // show the following message on screen.
-              $('.noBalance').text("You don't have enough balance for the pet adoption handling fee (please switch account or add more balance).").css("color", "red");
-              $('.panel-pet').eq(petId).find('button').text('Adopt').attr('disabled', true);
-          }
-      });
-    });
-
+    /*
+     * Replace me...
+     */
     var adoptionInstance;
 
     web3.eth.getAccounts(function(error, accounts) {
@@ -280,19 +308,70 @@ App = {
         // Execute adopt as a transaction by sending account
         return adoptionInstance.adopt(petId, {from: account});
       }).then(function(result) {
-        // Show the name of the pet the user have just adopted.
+        
+        // Show the name of the pet the user have just adopted. Arsh
         $('.alert').text('You have successfully adopted ' + petName + '!');
 
         return App.markAdopted();
       }).catch(function(err) {
-        // Enable button if there is error during processing
-        $(this).text('Adopt').removeAttr('disabled');
         console.log(err.message);
       });
     });
   },
 
-  // handle the voting process when user click the vote button.
+markReturned: function(adopters, account){
+ 
+	var adoptionInstance;
+
+	App.contracts.Adoption.deployed().then(function (instance) {
+	  adoptionInstance = instance;
+    
+
+	  return adoptionInstance.getAdopters.call();
+	})
+  .then(function(adopters){
+
+	  for (i=0;i<adopters.length;i++){
+	    if (adopters[i]=='0x0000000000000000000000000000000000000000') {
+        console.log('ahahahaah3');
+        $(".panel-pet").eq(i).find(".btn-adopt").text('Adopt').removeProp("disabled");
+        $(".panel-pet").eq(i).find(".btn-return").prop("disabled", true).removeClass("btn-danger");
+        $(".panel-pet").eq(i).find(".adopter-address").html('');
+    }
+	}
+  }).catch(function(err) {
+	console.log(err.message);
+});
+},
+//handleReturn
+handleReturn: function(event) {
+  event.preventDefault();
+
+  var petId = parseInt($(event.target).data('id'));
+
+  var returningInstance;
+
+  web3.eth.getAccounts(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+
+    var account = accounts[0];
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      returningInstance = instance;
+
+      // Execute adopt as a transaction by sending account
+      return returningInstance.returnPet(petId, {from: account});
+    }).then(function(result) {
+      return App.markReturned();
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  });
+},
+
+  // handle the voting process when user click the vote button. Arsh
   castVote: function() {
     // the candidate petID
     var candidateId = $('#candidatesSelect').val();
@@ -308,10 +387,13 @@ App = {
     });
   }
 
+
 };
 
 $(function() {
-  $(window).load(function() {
+
+  //$(window).load(function() { Arsh
+  $(document).ready(function() {
     App.init();
   });
 });
